9bff7611d19112d505fa5e8ca0e4c8850527b9d7
CamiloAcevedoCuevas <ccuevasa@uninorte.edu.co>
2024-11-10 21:11:32 -0500

    ..

--- ElPoderDeUnClick.py
+++ ElPoderDeUnClick.py
@@ -5,45 +5,45 @@ import cv2 # Video Management
 from tkinter import * # GUI
 import winsound # Audio Management
 
-class Escena:
-    """Escena del juego"""
-    def __init__(self, escena, indice):
-        self.escena = escena
-        self.indice = indice
+class Scene:
+    """Game Scene"""
+    def __init__(self, scene, index):
+        self.scene = scene
+        self.index = index
         self.next = None
 
-class Escenas:
-    """Lista de escenas"""
+class Scenes:
+    """Scenes"""
     def __init__(self):
         self.head = None
 
-    def add_escena(self, escena, indice):
-        """Añadir escena
+    def add_scene(self, scene, index):
+        """Add scene
 
         Args:
-            escena (VideoCapture): 
-            indice (int): 
+            scene (VideoCapture): 
+            index (int): 
         """
-        new_escena = Escena(escena, indice)
+        new_scene = Scene(scene, index)
         if self.head is None:
-            self.head = new_escena
+            self.head = new_scene
         else:
             current = self.head
             while current.next is not None:
                 current = current.next
-            current.next = new_escena
+            current.next = new_scene
 
-    def get_escena(self, indice):
-        """Mostrar escena
+    def get_scene(self, index):
+        """Play scene
 
         Args:
-            indice (int): 
+            index (int): 
         """
         current = self.head
         while current is not None:
-            if current.indice == indice:
+            if current.index == index:
                 while True:
-                    ret, frame = current.escena.read()
+                    ret, frame = current.scene.read()
                     if ret:
                         cv2.imshow('', frame)
                         if cv2.waitKey(15) == 27:
@@ -54,11 +54,11 @@ class Escenas:
             current = current.next
 
 def main():
-    escenas = Escenas()
+    scenes = Scenes()
 
-    for i in range(1, 3): # Buffering de escenas en memoria
-        escena = cv2.VideoCapture(f'escena{i}.mp4')
-        escenas.add_escena(escena, i)
+    for i in range(1, 3): # Scenes Buffering
+        scene = cv2.VideoCapture(f'scene{i}.mp4')
+        scenes.add_scene(scene, i)
 
     window = Tk()
 
@@ -72,8 +72,8 @@ def main():
     startLabel.place(x = 370, y = 100)
     def start():
         window.destroy()
-        winsound.PlaySound('audio1.wav', 0)
-        escenas.get_escena(1)
+        winsound.PlaySound('aud1.wav', 0)
+        scenes.get_scene(1)
     
     btnStart = Button(window, text = 'Start', fg = 'black', bg = 'white', font = ('Arial', 12), command = start)
     btnStart.place(x = 470, y = 200, width = 100, height = 30)
