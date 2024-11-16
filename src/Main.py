9ed7fb4463927588c0e1c547ce777c547d4f38b6
Camilo Acevedo <145441735+CamiloAcevedoCuevas@users.noreply.github.com>
2024-11-16 17:05:43 -0500

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -5,7 +5,7 @@ import cv2
 from tkinter import *
 from Scenes import Scenes
 from GUI import GUI
-from tkinter import ttk
+import winsound
 
 def main():
     scenes = Scenes()
@@ -18,17 +18,17 @@ def main():
     menu = Tk() # main menu
     gui.window = menu
     gui.set_window()
-    image = PhotoImage(file="assets/images/newyork.png")
-    label = ttk.Label(menu, image=image)
+    image = PhotoImage(file="assets/images/background.png")
+    label = ttk.Label(menu, image = image)
     label.place(relx = 0.5, rely = 0.5, anchor = CENTER)
-    gui.set_label('El Poder De Un Click', 22, 0.5, 0.35)
+    gui.set_label('El Poder De Un Click', 24, 0.5, 0.15)
     def start():
         scenes.scene = 'start'
         menu.destroy()
     def exit():
         menu.destroy()
-    gui.set_button('Iniciar Partida', start, 0.35, 0.55)
-    gui.set_button('Salir', exit, 0.65, 0.55)
+    gui.set_button('Iniciar Partida', start, 0.4, 0.8)
+    gui.set_button('Salir', exit, 0.6, 0.8)
     menu.mainloop()
 
     if scenes.scene == 'start':
@@ -43,82 +43,15 @@ def main():
             scenes.scene = 1
             start.destroy()
         gui.set_button('Continuar', scene1, 0.5, 0.85)
+        winsound.PlaySound('assets/audios/startaudio.wav', winsound.SND_ASYNC)
         start.mainloop()
 
         if scenes.scene == 1:
-            print("xd")
-
-
-    # if scenes.scene == 1:
-    #     scenes.get_scene(1) # charges scene 1
-    #     scene1 = Tk()
-    #     def scene2():
-    #         scenes.scene = 2
-    #         scene1.destroy()
-    #     def scene3():
-    #         scenes.scene = 3
-    #         scene1.destroy()
-    #     gui = GUI(scene1)
-    #     gui.set_window('Invertir Dinero', 'Si', 'No', scene2, scene3)
-    #     scene1.mainloop()
-
-    #     if scenes.scene == 2:
-    #         scenes.get_scene(2) # charges scene 2
-    #         scene2 = Tk()
-    #         def scene4():
-    #             scenes.scene = 4
-    #             scene2.destroy()
-    #             scenes.set_exit_scene() # charges scene 4
-    #         def scene5():
-    #             scenes.scene = 5
-    #             scene2.destroy()
-    #         gui = GUI(scene2)
-    #         gui.set_window('Llamar a la policía', 'Si', 'No', scene4, scene5)
-    #         scene2.mainloop()
-
-    #     elif scenes.scene == 3:
-    #         scenes.get_scene(3) # charges scene 3
-    #         scene3 = Tk()
-    #         def scene6():
-    #             scenes.scene = 6
-    #             scene3.destroy()
-    #             scenes.set_exit_scene() # charges scene 6
-    #         def scene7():
-    #             scenes.scene = 7
-    #             scene3.destroy()
-    #         gui = GUI(scene3)
-    #         gui.set_window('Abrir el correo', 'Si', 'No', scene6, scene7)
-    #         scene3.mainloop()
-
-    #     if scenes.scene == 5:
-    #         scenes.get_scene(5) # charges scene 5
-    #         scene5 = Tk()
-    #         def scene8():
-    #             scenes.scene = 8
-    #             scene5.destroy()
-    #             scenes.set_exit_scene() # charges scene 8
-    #         def scene9():
-    #             scenes.scene = 9
-    #             scene5.destroy()
-    #             scenes.set_exit_scene() # charges scene 9
-    #         gui = GUI(scene5)
-    #         gui.set_window('Aceptar la propuesta', 'Si', 'No', scene8, scene9)
-    #         scene5.mainloop()
-
-    #     elif scenes.scene == 7:
-    #         scenes.get_scene(7)
-    #         scene7 = Tk()
-    #         def scene10():
-    #             scenes.scene = 10
-    #             scene7.destroy()
-    #             scenes.set_exit_scene() # charges scene 10
-    #         def scene11():
-    #             scenes.scene = 11
-    #             scene7.destroy()
-    #             scenes.set_exit_scene() # charges scene 11
-    #         gui = GUI(scene7)
-    #         gui.set_window('Instalar el programa', 'Si', 'No', scene10, scene11)
-    #         scene7.mainloop()
-
+            scenes.get_scene(1)
+            scene1 = Tk()
+            gui.window = scene1
+            gui.set_window()
+            gui.
+            
 if __name__ == '__main__':
     main()
